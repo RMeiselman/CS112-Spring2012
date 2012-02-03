d2aa21789cb346aa4ce0a2b5f339302bc1e3d4a0
Ryan Meiselman <Dagerothmeiselman@gmail.com>
2012-02-03 12:20:14 -0500

    hw03 done

--- hw03/prissybot.py
+++ hw03/prissybot.py
@@ -1,48 +1,27 @@
 #!/usr/bin/env python
-"""
-prissybot.py
 
-CS112 Homework 3:   PrissyBot
+print "What is your name?"
+name=raw_input()
 
-Prissy bot, the rude chat bot, is just mean!  It does not listen, asks obnoxious questions, and says anything it likes.
-"""
+print "how are you today, ",name
+mood=raw_input()
+print "It's nice to know that you're",mood
 
-# Step 1:
-# -----------------------
-# Program the following.
-# 
-#    $ python prissybot.py
-#    Enter your name:  Paul
-#   
-#    PrissyBot: Hello there, Paul
-#    Paul: hi bot
-#    PrissyBot: You mean, "hi bot, sir!"
-# 
-# Make sure the user inputs their own name and responses.
+print "How much swag do you have, ",name
+swag=raw_input()
+print "Yo swag ain't shit, dog"
 
+print "yo dog I'ma reason with you right now. I don't like your face"
+something=raw_input()
+print "I DONT GIVE A SHIT WHAT YOU THINK, NOW LETS DO SOME MATH"
+n=raw_input("enter a number: ")
+m=raw_input("enter a number: ")
+n=int(n)
+m=float(m)
 
+print n,"+",m,"=",n+m
+print n,"-",m,"=",n-m
+print n,"*",m,"=",n*m
+print n,"/",m,"=",n/m
 
-# Step 2:
-# -----------------------
-# Keep adding to the conversation. Make sure that your program 
-# includes the following:
-# 
-#  * get and use input from the user
-#  * 3 math problems
-#     * at least one should get numbers from the user
-#  * at least 3 insults
-
-
-# Advanced
-# -------------------------
-# Make sure your prissy bot uses string formatting throughout.  
-# Also, create new programs for the following:
-#  
-#  1. draw some kind of ascii art based on user input
-#  2. print a decimal/binary/hexidecimal conversion table 
-#     * well formated and labeled
-#     * reads 5 numbers from the input (all less than 256)
-#  3. reduce a fraction
-#     * read a numerator and denominator from the user
-#     * ex.  6/4 = 1 2/4
-
+print "Now wasn't that just amazing? thats probably the best math you can do in that little brain of yours"
