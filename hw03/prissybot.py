7448006d215d1047600a5782c16b23a57fd0a92e
Ryan Meiselman <Dagerothmeiselman@gmail.com>
2012-02-05 16:38:39 -0500

    hw03 done

--- hw03/prissybot.py
+++ hw03/prissybot.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
 print "What is your name?"
-name=raw_input()
+name=raw_input("Please enter your name: ")
 
 print "how are you today, ",name
-mood=raw_input()
+mood=raw_input("Please enter your mood: ")
 print "It's nice to know that you're",mood
 
 print "How much swag do you have, ",name
-swag=raw_input()
+swag=raw_input("Please enter an amount of swag")
 print "Yo swag ain't shit, dog"
 
 print "yo dog I'ma reason with you right now. I don't like your face"
-something=raw_input()
+something=raw_input("Are you gonna put up with this?")
 print "I DONT GIVE A SHIT WHAT YOU THINK, NOW LETS DO SOME MATH"
 n=raw_input("enter a number: ")
 m=raw_input("enter a number: ")
