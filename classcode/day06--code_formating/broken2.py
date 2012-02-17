b91c5162bc06de4e74dc9b1f7b94475f0d254608
Ryan Meiselman <Dagerothmeiselman@gmail.com>
2012-02-17 13:00:53 -0500

    hw06 done

--- classcode/day06--code_formating/broken2.py
+++ classcode/day06--code_formating/broken2.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
 from random import randint
+
+input_number=int(raw_input())
+list1=[]
+
+#changes the list to include random number
+for _ in range(input_number):
+    list1.append(randint(0,20))
+
+#prints list a first time
+print list1
+
 s=1
-t=int(raw_input())
-rr=[]
-for _ in range(t):
-    rr.append(randint(0,20))
-print rr
 while s:
     s=0
-    for var in range(1,t):
-        if rr[var-1]>rr[var]:
-            t1=rr[i-1]
-            t2=rr[i]
-            rr[i-1]=t2
-            rr[i]=t1
+    for var in range(1,input_number):
+        if list1[var-1]>list1[var]:
+            input_number1=list1[var-1]
+            input_number2=list1[var]
+            list1[var-1]=input_number2
+            list1[var]=Input_number1
             s=1
-print rr
+#prints list a second time
+print list1
---
+++ hw06/prissybot.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+#Asks for the user's name
+print "What is your name?"
+name=raw_input("Please enter your name: ")
+
+#Asks for the user's mood
+print "how are you today, ",name
+mood = raw_input("Please enter your mood: ")
+print "It's nice to know that you're",mood
+
+#Asks how much swag the user has
+print "How much swag do you have, ",name
+swag = raw_input("Please enter an amount of swag: ")
+print "Yo swag ain't shit, dog"
+
+#Begins to textually chastise the user, and prompts the user for a response
+print "yo dog I'ma reason with you right now. I don't like your face. Come at me bro."
+answer = raw_input("Are you gonna put up with this?")
+print "I DONT GIVE A SHIT WHAT YOU THINK, NOW LETS DO SOME MATH"
+
+#Asks the user for 2 numbers
+n = raw_input("enter a number: ")
+m = raw_input("enter a number: ")
+n = int(n)
+m = float(m)
+
+#Does 4 equations with the user's numbers
+print n , "+" , m , "=" , n + m
+print n , "-" , m , "=" , n - m
+print n , "*" , m , "=" , n * m
+print n , "/" , m , "=" , n / m
+
+#One final insult from Prissybot
+print "Now wasn't that just amazing? thats probably the best math you can do in that little brain of yours"
